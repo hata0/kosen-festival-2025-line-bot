640242e2afed5a3da990ce18091430186dcfe44c
hata0 <haataa0100@gmail.com>
2025-10-26 03:33:27 +0900

    fix: \nが文字として解釈されてしまう不具合を修正

--- src/internal/infrastructure/config/api/spreadsheet/env-config.ts
+++ src/internal/infrastructure/config/api/spreadsheet/env-config.ts
@@ -3,6 +3,7 @@ import type { SpreadsheetApiConfig } from "./config";
 
 export class EnvSpreadsheetApiConfig implements SpreadsheetApiConfig {
   public readonly spreadsheetClientEmail: string = env.SPREADSHEET_CLIENT_EMAIL;
-  public readonly spreadsheetPrivateKey: string = env.SPREADSHEET_PRIVATE_KEY;
+  public readonly spreadsheetPrivateKey: string =
+    env.SPREADSHEET_PRIVATE_KEY.replace(/\\n/g, "\n");
   public readonly spreadsheetId: string = env.SPREADSHEET_ID;
 }
